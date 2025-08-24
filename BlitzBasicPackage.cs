c3634fc745a71d317600a0a970122d3348df152d
Simoxus <simoxusmanagement@gmail.com>
2025-08-23 18:48:49 -0600

    Add main package file

---
+++ BlitzBasicPackage.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace BlitzBasicLanguageSupport
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(BlitzBasicPackage.PackageGuidString)]
+    public sealed class BlitzBasicPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "186e229c-3ef2-46b3-b75a-7eea251afc03";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
