7e0281cf8a71ac5efc0fe1f13e61e1fe21fcc9a9
Simoxus <simoxusmanagement@gmail.com>
2025-08-23 18:48:04 -0600

    Add a class that checks if it's a *.bb file

---
+++ BlitzBasicContent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace BlitzBasicLanguageSupport
+{
+    internal static class BlitzBasicContent
+    {
+        [Export]
+        [Name("BlitzBasic")]
+        [BaseDefinition("code")]
+        internal static ContentTypeDefinition BlitzBasicContentTypeDefinition;
+
+        [Export]
+        [FileExtension(".bb")]
+        [ContentType("BlitzBasic")]
+        internal static FileExtensionToContentTypeDefinition BlitzBasicFileExtensionDefinition;
+    }
+}
