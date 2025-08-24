d10cb5241e75ee7a7cfe79f68a85def9f7bf675d
Simoxus <simoxusmanagement@gmail.com>
2025-08-23 18:48:59 -0600

    Add a class that defines syntax types

---
+++ BlitzBasicTypes.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace BlitzBasicLanguageSupport
+{
+    internal static class BlitzBasicClassificationDefinitions
+    {
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Keyword")]
+        internal static ClassificationTypeDefinition KeywordType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Comment")]
+        internal static ClassificationTypeDefinition CommentType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Type")]
+        internal static ClassificationTypeDefinition TypeType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Number")]
+        internal static ClassificationTypeDefinition NumberType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("String")]
+        internal static ClassificationTypeDefinition StringType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Function")]
+        internal static ClassificationTypeDefinition FunctionType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("BuiltIn")]
+        internal static ClassificationTypeDefinition BuiltInType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Blitz3DBuiltIn")]
+        internal static ClassificationTypeDefinition Blitz3D_BuiltInType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Preprocessor")]
+        internal static ClassificationTypeDefinition PreprocessorType = null;
+
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("Label")]
+        internal static ClassificationTypeDefinition LabelType = null;
+    }
+}
